a5b486a5209c72134156038eec32acdd9a38a5d6
Amna Ali <98670206+Amna-28@users.noreply.github.com>
2022-08-25 10:31:48 +0200

    Update maip-malaria.py

--- model/framework/code/maip-malaria.py
+++ model/framework/code/maip-malaria.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-from datetime import datetime
 import numpy as np
 import sys
 import os
@@ -18,7 +17,6 @@ file = {'input1': open('maip.csv', 'rb')}
 payload = { 'standardise': 'true','dl__ignore_cache': 'false'}
 
 r = requests.post(url, files=file, data = payload)
-print(r.status_code)
 
 soup = BeautifulSoup(r.text, features = 'html.parser')
 job_id = str(soup.text)
@@ -26,16 +24,13 @@ job_id = job_id.split(':')[1].strip().translate({ ord(c): None for c in "\"}" })
 
 download_url = 'http://www.ebi.ac.uk/chembl/interface_api/delayed_jobs/outputs/' + job_id + '/predictions.csv'
 download_response = requests.get(download_url,allow_redirects=True)
-print(download_response.status_code)
-#print("Job ID : ", job_id)
-
-now = datetime.now()
-#current_time = now.strftime("%H:%M:%S")
-#print("Current Time =", current_time)
 
 open( sys.argv[2] , "wb").write(download_response.content)
-print("Results saved in maip_predictions.csv")
 
+pred = pd.read_csv(sys.argv[2])
+pred = pred.drop(['id', 'standard_smiles'], axis=1)
+pred.rename(columns = {'smiles':'SMILES'}, inplace = True)
 
+pred.to_csv(sys.argv[2], index = False)
 
 
