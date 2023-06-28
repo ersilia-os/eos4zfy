1fd85f1bab0e93e9588208c410d5ebd393671bbf
emmakodes <emmamichael65@gmail.com>
2023-06-28 18:24:34 +0200

    refactor model

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,40 +1,61 @@
-# imports
-import os
-import csv
-import joblib
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+import csv
+import os
 
 # parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# current file directory
-root = os.path.dirname(os.path.abspath(__file__))
-
-# checkpoints directory
-checkpoints_dir = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
-
-# read checkpoints (here, simply an integer number: 42)
-ckpt = joblib.load(os.path.join(checkpoints_dir, "checkpoints.joblib"))
-
-# model to be run (here, calculate the Molecular Weight and add ckpt (42) to it)
-def my_model(smiles_list, ckpt):
-    return [MolWt(Chem.MolFromSmiles(smi))+ckpt for smi in smiles_list]
-    
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
     reader = csv.reader(f)
     next(reader) # skip header
     smiles_list = [r[0] for r in reader]
     
-# run model
-outputs = my_model(smiles_list, ckpt)
+# save smiles to a dataframe
+smiles = pd.DataFrame(smiles_list, columns=['smiles'])
+
+id_col = np.arange(0, len(smiles) ,1)
+id_col = [str(x) for x in id_col ]
+smiles.insert(0, column = 'id', value = id_col)
+
+smiles.to_csv('maip.csv', index=False)
+
+url = 'https://www.ebi.ac.uk/chembl/interface_api/delayed_jobs/submit/mmv_job'
+file = {'input1': open('maip.csv', 'rb')}
+payload = { 'standardise': True,'dl__ignore_cache': False}
+
+session = requests.Session()
+r = session.post(url, files=file, data = payload)
+
+soup = BeautifulSoup(r.text, features = 'html.parser')
+job_id = str(soup.text)
+job_id = job_id.split(':')[1].strip().translate({ ord(c): None for c in "\"}" })
+
+download_url = 'http://www.ebi.ac.uk/chembl/interface_api/delayed_jobs/outputs/' + job_id + '/predictions.csv'
+download_response = session.get(download_url,allow_redirects=True)
+
+output_file_temp = sys.argv[2]
+
+open(output_file_temp , "wb").write(download_response.content)
+
+pred = pd.read_csv(output_file_temp)
+
+# convert model_score to list
+outputs = pred['model_score'].tolist()
+
+
 
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"]) # header
+    writer.writerow(["model_score"]) # header
     for o in outputs:
         writer.writerow([o])
+        
+
+os.remove('maip.csv')
